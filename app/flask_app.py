350aaf2081205d126976673e3c520dde1da61097
warhammer21 <shreyakvashisht21@gmail.com>
2025-01-11 10:42:17 -0800

    fix the container to localhost port mapping issue

--- app/flask_app.py
+++ app/flask_app.py
@@ -46,5 +46,5 @@ def predict():
 
     # Return prediction as JSON
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=7000, debug=True)
